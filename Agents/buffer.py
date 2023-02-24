5287f8852f303157d399eba9926af358a5be522c
CCarissimo <cesarecarissimo@gmail.com>
2023-02-24 16:07:21 +0100

    adjust to share death memories, remove keep_death parameter

--- Agents/buffer.py
+++ Agents/buffer.py
@@ -4,14 +4,20 @@ from random import sample
 
 
 class ReplayMemory:
-    def __init__(self, max_size):
+    def __init__(self, max_size, len_death_memories):
         self.buffer = [None] * max_size
         self.max_size = max_size
         self.index = 0
         self.size = 0
+        self.death_memories = []
+        self.len_death_memories = len_death_memories
 
     def append(self, obj):
         self.buffer[self.index] = obj
+        if obj.terminal:
+            for i in range(0, self.len_death_memories):
+                ind = (self.index - i) % self.max_size
+                self.death_memories.append(self.buffer[ind])
         self.size = min(self.size + 1, self.max_size)
         self.index = (self.index + 1) % self.max_size
 
@@ -22,33 +28,43 @@ class ReplayMemory:
         S.append(self.buffer[self.index-1])
         return S
 
-    def reset(self, size_stories, keep_deaths=False):
-        if not keep_deaths:
-            tmp = []
-            if size_stories > 0:
-                sample = self.sample(size_stories)
-                for s in sample:
-                    tmp.append(s)
-            self.buffer = [None] * self.max_size
-            self.index = 0
-            self.size = 0
-            for i in range(len(tmp)):
-                self.append(tmp.pop())
-        else:
-            tmp = []
-            for i in range(len(self.buffer)):
-                transition = self.buffer[i]
-                if transition is None:
-                    continue
-                if transition.terminal:
-                    tmp.append(transition)
-            space = size_stories - len(tmp)
-            if space > 0:
-                sample = self.sample(space)
-                for s in sample:
-                    tmp.append(s)
-            self.buffer = [None] * self.max_size
-            self.index = 0
-            self.size = 0
-            for i in range(len(tmp)):
-                self.append(tmp.pop())
+    def reset(self):
+
+        self.buffer = [None] * self.max_size
+        self.index = 0
+        self.size = 0
+
+        for obj in self.death_memories:  # append them to buffer
+            self.buffer[self.index] = obj
+            self.size = min(self.size + 1, self.max_size)
+            self.index = (self.index + 1) % self.max_size
+
+        # if not keep_deaths:
+        #     tmp = []
+        #     if size_stories > 0:
+        #         sample = self.sample(size_stories)
+        #         for s in sample:
+        #             tmp.append(s)
+        #     self.buffer = [None] * self.max_size
+        #     self.index = 0
+        #     self.size = 0
+        #     for i in range(len(tmp)):
+        #         self.append(tmp.pop())
+        # else:
+        #     tmp = []
+        #     for i in range(len(self.buffer)):
+        #         transition = self.buffer[i]
+        #         if transition is None:
+        #             continue
+        #         if transition.terminal:
+        #             tmp.append(transition)
+        #     space = size_stories - len(tmp)
+        #     if space > 0:
+        #         sample = self.sample(space)
+        #         for s in sample:
+        #             tmp.append(s)
+        #     self.buffer = [None] * self.max_size
+        #     self.index = 0
+        #     self.size = 0
+        #     for i in range(len(tmp)):
+        #         self.append(tmp.pop())
--- death_experiments.py
+++ death_experiments.py
@@ -44,14 +44,14 @@ import pandas as pd
 import os
 import pickle
 
-MAX_STEPS = 10000
+MAX_STEPS = 100
 EPISODE_TIMEOUT = 10000
 GAMMA = 0.2
 ALPHA = 0.1
 BATCH_SIZE = 10
-size_stories_list = np.linspace(0, 1000, 31).astype(int)
+size_stories_list = [10]  # np.linspace(0, 100, 31).astype(int)
 buffer_size = 1000  # np.linspace(100, MAX_STEPS, 31).astype(int)
-REPETITIONS = 20
+REPETITIONS = 1
 
 cwd = os.getcwd()
 FOLDER = "%s" % cwd
@@ -70,76 +70,70 @@ initial_state = (0, 0)
 blacked_state = {}
 
 # UNCOMMENT IF YOU WOULD LIKE A PLOT OF THE GRIDWORLD BEFORE STARTING
-_, _, _, _ = Utils.plot_gridworld(grid, terminal_state, initial_state, blacked_state)
+# _, _, _, _ = Utils.plot_gridworld(grid, terminal_state, initial_state, blacked_state)
 
 master = []
 storage = {}
 
 for size_stories in size_stories_list:
     tmp = {}
-    for keep_deaths in [True, False]:
-        visits = []
-        n_tables = []
-        for iteration in range(REPETITIONS):
-            env = Environments.Gridworld(grid, terminal_state, initial_state, blacked_state)
-            states = [(i, j) for i in range(env.grid_height) for j in range(env.grid_width)]
-            env_shape = (env.grid_height, env.grid_width)
+    visits = []
+    n_tables = []
+    for iteration in range(REPETITIONS):
+        env = Environments.Gridworld(grid, terminal_state, initial_state, blacked_state)
+        states = [(i, j) for i in range(env.grid_height) for j in range(env.grid_width)]
+        env_shape = (env.grid_height, env.grid_width)
 
-            agent = Agents.SimpleNoveltor(ALPHA=ALPHA, GAMMA=GAMMA)
-            rb = Agents.ReplayMemory(max_size=buffer_size)
+        agent = Agents.SimpleNoveltor(ALPHA=ALPHA, GAMMA=GAMMA)
+        rb = Agents.ReplayMemory(max_size=buffer_size, len_death_memories=size_stories)
 
-            metrics, trajectory_metrics = Experiments.online_learning(MAX_STEPS, BATCH_SIZE, EPISODE_TIMEOUT, agent, rb,
-                                                                      env, states, env_shape, size_stories=size_stories,
-                                                                      keep_deaths=keep_deaths)
+        metrics, trajectory_metrics = Experiments.online_learning(MAX_STEPS, BATCH_SIZE, EPISODE_TIMEOUT, agent, rb,
+                                                                  env, states, env_shape)
 
-            trajectory_length_per_step = [metrics[t]["traj_len"] for t in range(MAX_STEPS)]
-            lifetime_per_agent = [trajectory_metrics[t]["lifetime"] for t in range(len(trajectory_metrics))]
+        trajectory_length_per_step = [metrics[t]["traj_len"] for t in range(MAX_STEPS)]
+        lifetime_per_agent = [trajectory_metrics[t]["lifetime"] for t in range(len(trajectory_metrics))]
 
-            # store final visits and n_tables
-            visits.append([[trajectory_metrics[n]['visits'][s] for s in states] for n in range(len(trajectory_metrics))])
-            n_tables.append([[trajectory_metrics[n]['n_table'][s] for s in states] for n in range(len(trajectory_metrics))])
+        # store final visits and n_tables
+        visits.append([[trajectory_metrics[n]['visits'][s] for s in states] for n in range(len(trajectory_metrics))])
+        n_tables.append([[trajectory_metrics[n]['n_table'][s] for s in states] for n in range(len(trajectory_metrics))])
 
-            # intergenerational differences of visits (states been) and n-table (q-learning of novelties)
-            visits_differences = [[trajectory_metrics[n + 1]['visits'][s] - trajectory_metrics[n]['visits'][s]
+        # intergenerational differences of visits (states been) and n-table (q-learning of novelties)
+        visits_differences = [[trajectory_metrics[n + 1]['visits'][s] - trajectory_metrics[n]['visits'][s]
+                               for s in states] for n in range(len(trajectory_metrics) - 1)]
+
+        n_table_differences = [[(trajectory_metrics[n + 1]['n_table'][s] - trajectory_metrics[n]['n_table'][s]).mean()
+                                for s in states] for n in range(len(trajectory_metrics) - 1)]
+
+        abs_visits_differences = [[abs(trajectory_metrics[n + 1]['visits'][s] - trajectory_metrics[n]['visits'][s])
                                    for s in states] for n in range(len(trajectory_metrics) - 1)]
 
-            n_table_differences = [[(trajectory_metrics[n + 1]['n_table'][s] - trajectory_metrics[n]['n_table'][s]).mean()
-                                    for s in states] for n in range(len(trajectory_metrics) - 1)]
+        abs_n_table_differences = [
+            [np.abs((trajectory_metrics[n + 1]['n_table'][s] - trajectory_metrics[n]['n_table'][s])).mean()
+             for s in states] for n in range(len(trajectory_metrics) - 1)]
 
-            abs_visits_differences = [[abs(trajectory_metrics[n + 1]['visits'][s] - trajectory_metrics[n]['visits'][s])
-                                       for s in states] for n in range(len(trajectory_metrics) - 1)]
+        # print(n_table_differences)
 
-            abs_n_table_differences = [
-                [np.abs((trajectory_metrics[n + 1]['n_table'][s] - trajectory_metrics[n]['n_table'][s])).mean()
-                 for s in states] for n in range(len(trajectory_metrics) - 1)]
+        results = {
+            "buffer_size": buffer_size,
+            "size_stories": size_stories,
+            "repetition": iteration,
+            "number_of_deaths": len(trajectory_metrics),
+            "lifetime_average": np.mean(lifetime_per_agent),
+            "lifetime_variance": np.var(lifetime_per_agent),
+            "visits_differences_mean": np.mean(visits_differences),
+            "abs_visits_differences_mean": np.mean(abs_visits_differences),
+            "visits_differences_var": np.var(visits_differences),
+            "n_table_differences_mean": np.mean(n_table_differences),
+            "abs_n_table_differences_mean": np.mean(abs_n_table_differences),
+            "n_table_differences_var": np.var(n_table_differences),
+        }
+        master.append(results)
 
-            # print(n_table_differences)
-
-            results = {
-                "buffer_size": buffer_size,
-                "size_stories": size_stories,
-                "keep_deaths": keep_deaths,
-                "repetition": iteration,
-                "number_of_deaths": len(trajectory_metrics),
-                "lifetime_average": np.mean(lifetime_per_agent),
-                "lifetime_variance": np.var(lifetime_per_agent),
-                "visits_differences_mean": np.mean(visits_differences),
-                "abs_visits_differences_mean": np.mean(abs_visits_differences),
-                "visits_differences_var": np.var(visits_differences),
-                "n_table_differences_mean": np.mean(n_table_differences),
-                "abs_n_table_differences_mean": np.mean(abs_n_table_differences),
-                "n_table_differences_var": np.var(n_table_differences),
-            }
-            master.append(results)
-
-        tmp[keep_deaths] = {"visits": visits, "n_tables": n_tables}
-
-    storage[size_stories] = tmp
+        tmp = {"visits": visits, "n_tables": n_tables}
+        with open(f"tables_storage_{size_stories}", "wb") as file:
+            pickle.dump(tmp, file)
 
 
 final_df = pd.DataFrame(master)
 print(final_df)
 final_df.to_csv(FOLDER + "/" + FILE_SIG + ".csv")
-
-with open("tables_storage", "wb") as file:
-    pickle.dump(storage, file)
